a1e207b8e344fdc1e17f60ecb9a0e91acb9a0a3b
Stefano Solidoro <kishosa@me.com>
2024-05-20 10:57:41 +0200

    Sistemati permessi

--- index.php
+++ index.php
@@ -10,11 +10,19 @@ $db = getDbInstance();
 $numRiparazioni = $db->getValue("riparazioni", "Sum(QTA)");
 $db->Where("stato", "IN ATTESA");
 $numDaCompletare = $db->getValue("lanci", "Sum(paia)");
+$db->Where("user_name", $_SESSION["username"]);
+$nome = $db->getValue("utenti", "nome");
 
 include_once ('includes/header.php');
 ?>
 
 <div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-6">
+            <h1 class="page-header page-action-links text-left">Benvenuto/a, <?php echo $nome; ?> </h1>
+        </div>
+    </div>
+    <hr>
     <?php
     $db = getDbInstance();
     $querySettings = "SELECT value FROM settings WHERE item = 'modulo_produzione'";
@@ -23,195 +31,29 @@ include_once ('includes/header.php');
     ?>
     <div class="row">
         <?php if ($_SESSION['admin_type'] !== 'operatore'): ?>
-            <?php if ($_SESSION['admin_type'] !== 'lavorante'): ?>
-                <div class="col-lg-12">
-                    <h1 class="page-header page-action-links text-left">Dashboard</h1>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="card bg-primary text-white">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-3">
-                                    <i class="fad fa-list-ol fa-5x"></i>
-                                </div>
-                                <div class="col-9 text-right">
-                                    <div class="h1">
-                                        <b>
-                                            <?php echo $numRiparazioni; ?>
-                                        </b>
-                                    </div>
-                                    <div>Riparazioni</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="../../functions/riparazioni/riparazioni.php"
-                            class="card-footer text-white bg-white text-primary">
-                            <span class="float-left ">Apri Elenco</span>
-                            <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
-                            <div class="clearfix"></div>
-                        </a>
-                    </div>
-                </div>
-                <?php if ($moduloProduzioneValue == 1): ?>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card bg-primary text-white bg-warning">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-3">
-                                        <i class="fad fa-shipping-timed fa-5x"></i>
-                                    </div>
-                                    <div class="col-9 text-right">
-                                        <div class="h1">
-                                            <b>
-                                                <?php echo !empty($numDaCompletare) ? $numDaCompletare : 0; ?>
-                                            </b>
-                                        </div>
-                                        <div>Da lanci da completare</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="../../functions/lanci/wait_lanci.php" class="card-footer text-white bg-white text-warning">
-                                <span class="float-left">Apri Elenco</span>
-                                <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </a>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            <?php endif ?>
+            <?php if ($_SESSION['admin_type'] !== 'lavorante'):
+                include ("includes/dashboard/dash_amministrazione.php");
+            endif ?>
         <?php endif ?>
     </div>
 
     <!-- TAB RIPARAZIONI solo per LAVORANTE -->
-    <?php
-    if ($tipoUtente == "lavorante") {
-        // Esegui una query SQL per ottenere la somma dei campi richiesti
-        $db = getDbInstance();
-        $username = $_SESSION['username'];
-        $query = "SELECT SUM(QTA) AS sumQTA
-              FROM riparazioni
-              JOIN lab_user ON riparazioni.LABORATORIO = lab_user.lab
-              WHERE lab_user.user = '$username'";
-        $result = $db->rawQuery($query);
-        $sumQTA = (empty($result) || $result[0]['sumQTA'] === null) ? 0 : $result[0]['sumQTA'];
-
-        echo '<div class="col-lg-12">
-            <h1 class="page-header">Dashboard</h1>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="card bg-danger text-white">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-3">
-                            <i class="fad fa-copy fa-5x"></i>
-                        </div>
-                        <div class="col-9 text-right">
-                            <div class="h1"><b>' . $sumQTA . '</div>
-                            <div>Riparazioni assegnate</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="../../functions/lab_riparazioni/lab_riparazioni.php" class="card-footer text-white">
-                    <span class="float-left">Apri Elenco</span>
-                    <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
-                    <div class="clearfix"></div>
-                </a>
-            </div>
-        </div>';
-    }
-    ?>
-    <!-- FINE TAB RIPARAZIONI solo per LAVORANTE -->
-    <?php
-    if ($tipoUtente == "lavorante"): ?>
+    <div class="row">
         <?php
-        $db = getDbInstance();
-        $username = $_SESSION['username'];
-        $query = "SELECT laboratori.ID
-            FROM laboratori
-            WHERE laboratori.Nome = (
-                SELECT lab_user.lab
-                FROM lab_user
-                WHERE lab_user.user = '$username'
-            )";
-        $result = $db->rawQuery($query);
-        $idLab = $result[0]['ID'];
-
-        $query = "SELECT SUM(paia) AS sumQTA
-              FROM lanci
-              WHERE lanci.id_lab = '$idLab' AND stato != 'IN ATTESA'";
-        $result = $db->rawQuery($query);
-        $sumQTA = (empty($result) || $result[0]['sumQTA'] === null) ? 0 : $result[0]['sumQTA'];
+        if ($tipoUtente == "lavorante") {
+            // Esegui una query SQL per ottenere la somma dei campi richiesti
+            include ("includes/dashboard/dash_lavorante.php");
+        }
         ?>
-        <div class="col-lg-3 col-md-6">
-            <div class="card bg-success text-white">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-3">
-                            <i class="fad fa-tasks fa-5x"></i>
-                        </div>
-                        <div class="col-9 text-right">
-                            <b>
-                                <div class="h1">
-                                    <?php echo $sumQTA; ?>
-                                </div>
-                                <div>Lanci in lavoro</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <a href="../../functions/lab_lanci/lab_lanci.php" class="card-footer text-white">
-                <span class="float-left">Gestici Avanzamento</span>
-                <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
-                <div class="clearfix"></div>
-            </a>
-        </div>
     </div>
 
-    <?php
-    $db = getDbInstance();
-    $username = $_SESSION['username'];
-    $query = "SELECT laboratori.ID
-            FROM laboratori
-            WHERE laboratori.Nome = (
-                SELECT lab_user.lab
-                FROM lab_user
-                WHERE lab_user.user = '$username'
-            )";
-    $result = $db->rawQuery($query);
-    $idLab = $result[0]['ID'];
-
-    $query = "SELECT SUM(paia) AS sumQTA
-              FROM lanci
-              WHERE lanci.id_lab = '$idLab' AND stato = 'IN ATTESA'";
-    $result = $db->rawQuery($query);
-    $sumQTA = (empty($result) || $result[0]['sumQTA'] === null) ? 0 : $result[0]['sumQTA'];
-    ?>
-    <div class="col-lg-3 col-md-6">
-        <div class="card bg-warning text-white">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-3">
-                        <i class="fad fa-clock fa-5x"></i>
-                    </div>
-                    <div class="col-9 text-right">
-                        <b>
-                            <div class="h1">
-                                <?php echo $sumQTA; ?>
-                            </div>
-                            <div>Lanci in fase di preparazione</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <a href="../../functions/lab_lanci/lab_wait_lanci.php" class="card-footer text-white">
-            <span class="float-left">Controlla</span>
-            <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
-            <div class="clearfix"></div>
-        </a>
+    <div class="row">
+        <?php
+        if ($tipoUtente == "lavorante"):
+            include ("includes/dashboard/dash_lavorante2.php");
+        endif ?>
     </div>
-    </div>
-<?php endif ?>
-
+    <!-- FINE TAB RIPARAZIONI solo per LAVORANTE -->
 </div>
 
 </div>
